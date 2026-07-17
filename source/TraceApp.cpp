30d45b25d0e5423dc84c952c3af25ea6d00d6e9e
Alrecenk <Alrecenk@gmail.com>
2026-07-17 13:10:12 -0700

    fix comment

--- source/TraceApp.cpp
+++ source/TraceApp.cpp
@@ -74,10 +74,10 @@ void TraceApp::run() {
 		particles->setColor(mouse_particle_id, glm::vec4(0, 0, 1, 1)); // blue
 		mouse_position = window->window_target->camera_position + window->getMouseRay() * t; // hit postion
 	}
-	else if(bt > 0) { // no collision
+	else if(bt > 0) { //collides only with bounding box
 		particles->setColor(mouse_particle_id, glm::vec4(0.7f, 0, .7f, 1)); // purple
 		mouse_position = window->window_target->camera_position + window->getMouseRay() * bt; // hit boundikng box
-	}else{
+	}else{ // no collision
 		particles->setColor(mouse_particle_id, glm::vec4(1, 0, 0, 1)); // red
 		mouse_position = window->window_target->camera_position + window->getMouseRay() * 3.0f; // arbitrary depth on no collision
 	}
