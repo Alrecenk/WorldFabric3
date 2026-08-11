7239fe2048629993641127808403719215928b6e
Alrecenk <Alrecenk@gmail.com>
2026-08-11 03:08:30 -0400

    Fix older shaders for GLTF vertex color

--- source/Main.cpp
+++ source/Main.cpp
@@ -392,26 +392,23 @@ void setupGameStates() {
 	WorldPlugin* worlds = getTool<WorldPlugin>();
 	StatePlugin* app = getTool<StatePlugin>();
 
-
 	//app->add(VulkanDemoApp::state_name, std::shared_ptr<VulkanDemoApp>(new VulkanDemoApp()));
 	//app->setState(VulkanDemoApp::state_name);
+
+	//app->add(BallTestApp::state_name, std::shared_ptr<BallTestApp>(new BallTestApp()));
+	//app->setState(BallTestApp::state_name);
 
 	//app->add(SceneDemoApp::state_name, std::shared_ptr<SceneDemoApp>(new SceneDemoApp()));
 	//app->setState(SceneDemoApp::state_name);
 
 	//app->add(SceneDemoApp2::state_name, std::shared_ptr<SceneDemoApp2>(new SceneDemoApp2()));
 	//app->setState(SceneDemoApp2::state_name);
-
-	//app->add(BallTestApp::state_name, std::shared_ptr<BallTestApp>(new BallTestApp()));
-	//app->setState(BallTestApp::state_name);
-
 	
-
 	//app->add(SocketTest::state_name, std::shared_ptr<SocketTest>(new SocketTest()));
 	//app->setState(SocketTest::state_name);
 
-	app->add(BallThrowApp::state_name, std::shared_ptr<BallThrowApp>(new BallThrowApp()));
-	app->setState(BallThrowApp::state_name);
+	//app->add(BallThrowApp::state_name, std::shared_ptr<BallThrowApp>(new BallThrowApp()));
+	//app->setState(BallThrowApp::state_name);
 
 	//app->add(MirrorApp::state_name, std::shared_ptr<MirrorApp>(new MirrorApp()));
 	//app->setState(MirrorApp::state_name);
@@ -422,8 +419,8 @@ void setupGameStates() {
 	//app->add(ConstraintTestApp::state_name, std::shared_ptr<ConstraintTestApp>(new ConstraintTestApp()));
 	//app->setState(ConstraintTestApp::state_name);
 
-	//app->add(CollisionTestApp::state_name, std::make_shared<CollisionTestApp>());
-	//app->setState(CollisionTestApp::state_name);
+	app->add(CollisionTestApp::state_name, std::make_shared<CollisionTestApp>());
+	app->setState(CollisionTestApp::state_name);
 }
 
 int debugMain(int argc, char* argv[]) {
